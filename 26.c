ce74c5dc0cc692465b72ca311d5a201b7cbbe582
박민성 <102217591+minsung060607@users.noreply.github.com>
2023-02-24 16:29:14 +0900

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int num1;
+    scanf("%d",&num1);
+    for(int i=1;i<=num1/4;i++){
+        printf("long ");
+    }
+    printf("int");
+    
+    return 0;
+}
