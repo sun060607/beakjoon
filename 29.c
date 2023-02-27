4a002ba2d7741c6f1dbb5ef0e116a60a02a48f25
박민성 <102217591+minsung060607@users.noreply.github.com>
2023-02-27 09:01:47 +0900

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int num1,num2,num3;
+    scanf("%d",&num1);
+    for(int i=1;i<=num1;i++){
+        scanf("%d %d",&num2,&num3);
+        printf("Case #%d: %d + %d = %d\n",i,num2,num3,num2+num3);
+    }
+    
+    
+    return 0;
+}
