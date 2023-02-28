050d2631ce5816e651c6f195c367bb27850a0d59
박민성 <102217591+minsung060607@users.noreply.github.com>
2023-02-28 09:35:40 +0900

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num1;
+    scanf("%d",&num1);
+    for(int i=1;i<=num1;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
